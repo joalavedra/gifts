a5ac402703b6dcf8f9fe5fb09d3856eae85fca09
joalavedra <62625514+joalavedra@users.noreply.github.com>
2025-01-05 12:55:47 +0100

    Adding abi SC

--- lib/contracts/abis/giftToken.ts
+++ lib/contracts/abis/giftToken.ts
@@ -1,23 +1,33 @@
 export const giftTokenAbi = [
   {
     inputs: [
-      { name: "to", type: "address" },
-      { name: "id", type: "uint256" },
-      { name: "amount", type: "uint256" },
+      { name: "receiver", type: "address" },
+      { name: "tokenId", type: "uint256" },
+      { name: "quantity", type: "uint256" },
+      { name: "currency", type: "address" },
+      { name: "pricePerToken", type: "uint256" },
+      { name: "allowlistProof", type: "tuple", 
+        components: [
+          { name: "proof", type: "bytes32[]" },
+          { name: "quantityLimitPerWallet", type: "uint256" },
+          { name: "pricePerToken", type: "uint256" },
+          { name: "currency", type: "address" }
+        ]
+      },
       { name: "data", type: "bytes" }
     ],
-    name: "mint",
+    name: "claim",
     outputs: [],
     stateMutability: "payable",
     type: "function"
   },
   {
     inputs: [
-      { name: "from", type: "address" },
-      { name: "id", type: "uint256" },
-      { name: "amount", type: "uint256" }
+      { name: "account", type: "address[]" },
+      { name: "ids", type: "uint256[]" },
+      { name: "values", type: "uint256[]" }
     ],
-    name: "burn",
+    name: "burnBatch",
     outputs: [],
     stateMutability: "nonpayable",
     type: "function"
